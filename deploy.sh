240bca2913f335ed70ac214e56c53f82d44b1f62
vaisaliarjun <124667710+vaisaliarjun@users.noreply.github.com>
2025-03-19 15:03:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set Docker image name, tag, and container name
-IMAGE_NAME="vaishu273/docker"
+IMAGE_NAME="vaishu273/devops"
 TAG="latest"
 CONTAINER_NAME="jenkins-docker-container"
 
